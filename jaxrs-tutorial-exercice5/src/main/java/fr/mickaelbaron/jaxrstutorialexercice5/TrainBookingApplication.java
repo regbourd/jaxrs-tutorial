c6027b29133df051bd608ce21fc2d53c017d42e7
Reginald.Bourdon <Reginald.Bourdon@LIN0121600.pdt.platine.org>
2020-02-19 16:45:53 +0100

    corr exo 5 jaxrs

---
+++ jaxrs-tutorial-exercice5/src/main/java/fr/mickaelbaron/jaxrstutorialexercice5/TrainBookingApplication.java
@@ -0,0 +1,14 @@
+package fr.mickaelbaron.jaxrstutorialexercice5;
+
+import java.util.logging.Level;
+
+import org.glassfish.jersey.logging.LoggingFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class TrainBookingApplication extends ResourceConfig {
+
+    public TrainBookingApplication() {
+        this.registerClasses(TrainBookingResource.class, TrainResource.class);
+        this.property(LoggingFeature.LOGGING_FEATURE_LOGGER_LEVEL_SERVER, Level.WARNING.getName());
+    }
+}
